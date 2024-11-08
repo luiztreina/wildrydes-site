cc3ff36e070c8a6553c5bb385968440754f89105
luiztreina <86146790+luiztreina@users.noreply.github.com>
2024-11-08 00:51:41 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_gCu1U5iK2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '77ki0q5g0c5pkmat0mb21sv221', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
